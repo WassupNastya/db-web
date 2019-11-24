f1b93396582350513425c500f3a1e6ab490d0c81
Anastasia Kogteva <kogteva.nastya@gmail.com>
2019-11-24 19:04:08 +0300

    init commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+const getItems = () => {
+  fetch('http://localhost:3000/crud')
+    .then(response => response.json())
+    .then(items => console.log(items))
+    .catch(err => console.log(err))
+}
+
+function App() {
+  getItems()
+  return (
+    <div></div>
+  );
+}
+
+export default App;
