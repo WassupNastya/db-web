142e3d12e9b1c5b2e9a7b3f20c35879c43eef4d2
Anastasia Kogteva <kogteva.nastya@gmail.com>
2019-12-01 11:46:54 +0300

    change titles & add rows

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 import Header from "./Header";
 import ButtonGroup from "./ButtonGroup";
+import Row from "./Row";
 
 const getItems = () => {
-  fetch("http://localhost:3000/crud", {surname: 'Ivanov'})
+  fetch("http://localhost:3000/crud", { surname: "Ivanov" })
     .then(response => response.json())
     .then(items => console.log(items))
     .catch(err => console.log(err));
 };
 
+const items = [
+  {
+    id: 1,
+    surname: "Ivanov",
+    name: "Ivan"
+  },
+  {
+    id: 2,
+    surname: "Sidorov",
+    name: "Sergey"
+  },
+  {
+    id: 3,
+    surname: "Sidorov",
+    name: "Sergey"
+  },
+  {
+    id: 4,
+    surname: "Sidorov",
+    name: "Sergey"
+  },
+  {
+    id: 5,
+    surname: "Sidorov",
+    name: "Sergey"
+  },
+  {
+    id: 6,
+    surname: "Sidorov",
+    name: "Sergey"
+  },
+  {
+    id: 7,
+    surname: "Sidorov",
+    name: "Sergey"
+  },
+  {
+    id: 8,
+    surname: "Sidorov",
+    name: "Sergey"
+  }
+];
+
 function App() {
+  const [title, setTitle] = useState("Candidates");
+
   getItems();
   return (
     <div>
       <div className="main-screen">
-        <Header></Header>
+        <Header changeTitle={(title) => setTitle(title)}></Header>
         <div className="d-flex justify-content-between">
-        <h2 className="page-title">Candidates</h2>
-        <ButtonGroup></ButtonGroup>
+          <h2 className="page-title">{title}</h2>
+          <ButtonGroup></ButtonGroup>
+        </div>
+        <div className="table-background">
+          {items.map(x => (
+            <Row key={x.id}></Row>
+          ))}
         </div>
       </div>
     </div>
--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,16 +1,48 @@
-import React, { useState } from 'react';
-import './Header.css';
+import React, { useState } from "react";
+import "./Header.css";
 
-function Header() {
-  const [model, setModel] = useState('0');
+function Header({changeTitle, ...props}) {
+  const [model, setModel] = useState(0);
 
   return (
     <div className="header">
-        <a className={model == 0? "active-link" : "link"} href="#" onClick={() => setModel(0)}>Candidates</a>
-        <a className={model == 1? "active-link" : "link"} href="#" onClick={() => setModel(1)}>Abstracts</a>
-        <a className={model == 2? "active-link" : "link"} href="#" onClick={() => setModel(2)}>Offers</a>
-        <a className={model == 3? "active-link" : "link"} href="#" onClick={() => setModel(3)}>Reviews</a>
-        <a className={model == 4? "active-link" : "link"} href="#" onClick={() => setModel(4)}>Interviews</a>
+      <a
+        className={model === 0 ? "active-link" : "link"}
+        href="/#"
+        onClick={() => {
+          setModel(0); changeTitle("Candidates");
+        }}
+      >
+        Candidates
+      </a>
+      <a
+        className={model === 1 ? "active-link" : "link"}
+        href="/#"
+        onClick={() => {setModel(1); changeTitle('Abstracts')}}
+      >
+        Abstracts
+      </a>
+      <a
+        className={model === 2 ? "active-link" : "link"}
+        href="/#"
+        onClick={() => {setModel(2); changeTitle('Offers')}}
+      >
+        Offers
+      </a>
+      <a
+        className={model === 3 ? "active-link" : "link"}
+        href="/#"
+        onClick={() => {setModel(3); changeTitle('Reviews')}}
+      >
+        Reviews
+      </a>
+      <a
+        className={model === 4 ? "active-link" : "link"}
+        href="/#"
+        onClick={() => {setModel(4); changeTitle('Interviews')}}
+      >
+        Interviews
+      </a>
     </div>
   );
 }
